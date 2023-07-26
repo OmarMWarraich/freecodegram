089e5f80e9762782f73644df6ac6b2a6d1262c93
OmarMWarraich <earthling986@gmail.com>
2023-07-26 11:38:34 +0500

    feat: Profile Page UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <div style="width:200px;height:200px;">
+                <img src="jpg/profile.jpg" style="width:100%;height:100%;" class="rounded-circle" alt="image">
             </div>
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>omw786</h1>
+            </div>
+            <div class="d-flex">
+                <div style="padding-right:2rem;"><strong>24</strong> posts</div>
+                <div style="padding-right:2rem;"><strong>17</strong> followers</div>
+                <div style="padding-right:2rem;"><strong>40</strong> following</div>
+            </div>
+            
+            <div class="pt-4"><strong>Omar Mohammad</strong></div>
+            <div>BlockChain Developer</div>
+            <div>Full Stack Developer</div>
+            <div><a href="#">https://o-v-a-portfolio.netlify.app/</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="jpg/profile.jpg" alt="image">
+        </div>
+        <div class="col-4">
+            <img src="jpg/profile.jpg" alt="image">
+        </div>
+        <div class="col-4">
+            <img src="jpg/profile.jpg" alt="image">
         </div>
     </div>
 </div>
