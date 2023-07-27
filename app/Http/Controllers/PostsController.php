96c74a3503ad78aa9f5880e59e3c1d14507408fa
OmarMWarraich <earthling986@gmail.com>
2023-07-27 14:05:50 +0500

    feat: Middleware Authentication

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
-{
+{   
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function create()
     {
         return view('posts.create');
@@ -18,8 +22,7 @@ class PostsController extends Controller
             'image' => ['required', 'image'],
         ]);
 
-        
-        \App\Models\Post::create($data);
+        auth()->user()->posts()->create($data);
         dd(request()->all());
     }
 }
